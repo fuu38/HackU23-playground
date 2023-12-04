1e495498882d8f924d93025b5a7e3cd8a0a9f5c6
fuu38 <contact@fumiya.biz>
2023-12-04 16:25:27 +0900

    init lib-stack.ts

---
+++ lib/hack_u23-playground-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HackU23PlaygroundStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    // Create a simple API sample
+    
+  }
+}
